e706a55e25edaf8d7c040e532f60057b0c854190
Tanmay Upreti <iamtanmayupreti@gmail.com>
2023-04-18 18:43:42 +0530

    Added Facilities Section

---
+++ js/facilites.js
@@ -0,0 +1,7 @@
+let facilities = [
+  { name: `Library`, about: `1` },
+  { name: `Labs`, about: `5` },
+  { name: `Classrooms`, about: `59` },
+  { name: `Teachers`, about: `♾️` },
+  // { name: `Facility C`, about: `<i class="fas fa-check-circle"></i>` }
+];
--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 console.log(`New Beersheba Public School`);
 
+// Theme Toggle
+
 let theme = document.querySelector("#theme");
 
 theme.onclick = () => {
@@ -10,3 +12,18 @@ theme.onclick = () => {
     document.body.classList.remove("dark");
   }
 };
+
+// Facilities Table
+
+let table = document.getElementById("table");
+
+for (let i = 0; i < facilities.length; i++) {
+  let row = table.insertRow();
+  let cell1 = row.insertCell(0);
+  let cell2 = row.insertCell(1);
+  // let cell3 = row.insertCell(2);
+
+  cell1.innerHTML = facilities[i].name;
+  cell2.innerHTML = facilities[i].about;
+  // cell3.innerHTML = facilities[i].capacity;
+}
